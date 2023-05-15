bebe9fd8a8cca8841ca2681e83a4c120d9596417
houyuxi <yuxi.hou@transwarp.io>
2023-05-16 01:07:09 +0800

    init project development environment

---
+++ rollup.config.mjs
@@ -0,0 +1,94 @@
+import nodeResolve from "@rollup/plugin-node-resolve" ;
+import commonjs from "@rollup/plugin-commonjs" ;
+import typescript from "rollup-plugin-typescript2" ;
+import clear from "rollup-plugin-clear" ;
+import rollupPluginDts from "rollup-plugin-dts";
+import json from "@rollup/plugin-json" ;
+import path from "path";
+import * as url from "url";
+import fs from "fs-extra"
+import externals from "rollup-plugin-node-externals"
+
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+console.log(__dirname);
+const resolve = p => path.resolve(__dirname, p);
+
+const packageJson = fs.readJSONSync(resolve("package.json"))
+const tsconfig = resolve("tsconfig.json")
+const tsconfigJson = fs.readJSONSync(tsconfig)
+
+
+const input = "./src/index.ts"
+const {
+    outDir: outputDir,
+    sourceMap: sourcemap
+} = tsconfigJson["compilerOptions"]
+
+const {
+    name: pkgName,
+    types: declarationFile
+} = packageJson
+let name = path.basename(pkgName).replaceAll("-", "_")
+
+const Format = {
+    ES: "es",
+    CJS: "cjs",
+    IIFE: "iife",
+}
+const emitFormats = [
+    Format.ES,
+    Format.CJS,
+    Format.IIFE
+]
+const outputFileMapper = {
+    [Format.ES]: "index.mjs",
+    [Format.CJS]: "index.cjs",
+    [Format.IIFE]: "index.iife.js"
+}
+
+const basePlugins = [
+    clear({
+        targets: ["dist"],
+    }),
+    json(),
+    nodeResolve({
+        preferBuiltins: false,
+    }),
+    commonjs(),
+    typescript({
+        tsconfig,
+    }),
+    externals()
+];
+
+export default [{
+    input,
+    output: emitFormats.map(format => {
+        return {
+            format,
+            file: resolve(path.join(outputDir, outputFileMapper[format])),
+            name,
+            exports: "named",
+            sourcemap,
+        }
+    }),
+    plugins: basePlugins,
+
+},
+    {
+        input,
+        output: [{
+            file: resolve(declarationFile),
+            format: "es"
+        }],
+        plugins: [
+            ...basePlugins,
+            rollupPluginDts({
+                tsconfig
+            })
+        ],
+        external: ["type-fest"]
+    }
+];
